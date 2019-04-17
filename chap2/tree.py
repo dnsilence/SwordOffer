7a912d338a7dde7dbaf685824b9ad5ba1fb9b252
dnsilence <DN_Silence@hotmail.com>
2019-04-17 16:21:10 +0800

    pre-order,in-order and post-order

--- chap2/tree.py
+++ chap2/tree.py
@@ -9,9 +9,58 @@ class Tree(object):
         self.left = left
         self.right = right
 
+    def __pre_order(self, tree):
+        """前序遍历内部函数"""
+        if tree is not None:
+            print(tree.root)
+            self.__pre_order(tree.left)
+            self.__pre_order(tree.right)
 
-def pre_order(tree):
-    if tree.root is not None:
-        print(tree.root)
-        pre_order(tree.left)
-        pre_order(tree.right)
+    def pre_order(self):
+        """实现前序遍历，因为递归传入有可能为空，这里没有采用设定默认值判断为空
+        的方式设置类方法，而是采用两个函数，以下遍历方法同理"""
+        print("Pre order:")
+        self.__pre_order(self)
+
+    def __in_order(self, tree):
+        """中序遍历内部函数"""
+        if tree is not None:
+            self.__in_order(tree.left)
+            print(tree.root)
+            self.__in_order(tree.right)
+
+    def in_order(self):
+        """实现中序遍历"""
+        print("In order:")
+        self.__in_order(self)
+
+    def __post_order(self, tree):
+        """后序遍历内部函数"""
+        if tree is not None:
+            self.__post_order(tree.left)
+            self.__post_order(tree.right)
+            print(tree.root)
+
+    def post_order(self):
+        """实现后序遍历"""
+        print("Post order:")
+        self.__post_order(self)
+
+
+if __name__ == '__main__':
+    tree = Tree('D',
+                Tree('B',
+                     Tree('A'),
+                     Tree('C')
+                     ),
+                Tree('E',
+                     None,
+                     Tree('G',
+                          Tree('F')
+                          )
+                     )
+                )
+
+    tree.pre_order()
+    tree.in_order()
+    tree.post_order()
