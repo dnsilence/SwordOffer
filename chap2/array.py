44a257067b65e9bc758707feee2bff7ba7e8508b
dnsilence <DN_Silence@hotmail.com>
2019-04-28 20:06:53 +0800

    classification

--- chap2/array.py
+++ chap2/array.py
@@ -2,6 +2,7 @@
 
 
 def duplicate(numbers, duplication):
+    """查找数组中重复的元素"""
     # 输入检查
     if numbers is None or len(numbers) <= 1:
         return False
@@ -31,10 +32,35 @@ def duplicate(numbers, duplication):
     return False
 
 
+def find(matrix, number):
+    """查找二维数组中某个元素"""
+    if matrix is not None:
+        rows = len(matrix)
+        columns = len(matrix[0])
+        # 从右上角开始查询
+        row = 0               # 从第0行开始
+        column = columns - 1  # 从第column-1列开始
+        while row <= (rows-1) and column >= 0:
+            if matrix[row][column] < number:       # 当前值小于寻找值
+                row += 1  # 往下找
+            elif matrix[row][column] > number:     # 当前值大于寻找值
+                column -= 1  # 往左找
+            else:
+                return True, [row, column]
+    return False, [0, 0]
+
+
 if __name__ == '__main__':
+    # 查找数组中重复的元素
     # numbers = [2, 3, 1, 0, 2, 5, 3]     # 数组中包含一个或多个重复数字
     numbers = []     # 无效输入
     numbers = [1, 2, 3, 4, 5, 6]    # 不包括重复数字
     duplication = [0]
     print(duplicate(numbers, duplication), duplication[0])
+
+    # 查找二维数组中某一元素
+    # matrix = [[1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 7, 10]]
+    matrix = [[]]
+    a, b = find(matrix, 19)
+    print(a, b)
 
