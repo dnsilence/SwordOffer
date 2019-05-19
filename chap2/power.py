b0ea3b19369d5ffa1df891a26ae33f273a753848
dnsilence <DN_Silence@hotmail.com>
2019-05-19 14:50:49 +0800

    power

---
+++ chap2/power.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+
+
+def power(base, exponent):
+    try:
+        result = power_with_unsigned_exponent(base, abs(exponent))
+        if exponent < 0:
+            return 1/result
+        else:
+            return result
+    except ZeroDivisionError:
+        print("Error: base is zero and exponet is negative.")
+        return None
+
+
+def power_with_unsigned_exponent(base, exponent):
+    if exponent == 0:
+        return 1
+    if exponent == 1:
+        return base
+
+    result = power_with_unsigned_exponent(base, exponent >> 1)
+    result *= result
+    if exponent & 1 == 1:
+        result *= base
+
+    return result
+
+
+if __name__ == '__main__':
+    array = [-2, 2, 0]
+    for base in array:
+        for exponent in array:
+            print(power(base, exponent))
